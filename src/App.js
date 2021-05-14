18dfe5cad31d721e780c1e703d3cd2f9528ff30b
Jose <joseossorio99@hotmail.com>
2021-05-13 20:38:37 -0500

    add routing for App.js and pages folder with Page components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Header from "./components/Layout/Header";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import Profile from "./pages/Profile";
+import RankPage from "./pages/RankPage";
 
 const App = () => {
     return (
-        <div>
-          <Header />
-        </div>
+        <Layout>
+            <Switch>
+                <Route path={"/"} exact>
+                    <HomePage />
+                </Route>
+                <Route path={"/auth"}>
+                    <AuthPage />
+                </Route>
+                <Route path={"/profile"}>
+                    <Profile />
+                </Route>
+                <Route path={"/rank"}>
+                    <RankPage />
+                </Route>
+                <Route path={"*"}>
+                    <Redirect to={"/"} />
+                </Route>
+            </Switch>
+        </Layout>
     );
 };
 
